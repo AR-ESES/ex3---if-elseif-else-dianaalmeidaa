b0b713ce87e62e614dc21f8aed182a9cad2b93c8
dianaalmeidaa <diana.p.almeida1@gmail.com>
2023-05-30 09:09:37 +0100

    Código

--- sketch.js
+++ sketch.js
@@ -1,17 +1,36 @@
-let laranjas;
-
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0)
+    createCanvas(windowWidth, windowHeight);
+    background(0)
 
 }
 
 function draw() {
 
-	stroke(230);
-	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+    background(0)
+
+
+if(mouseY >= height/2) {
+    noFill();
+}
+
+else{
+
+    fill(12,108,57); //verde
+}
+
+
+if(mouseX <= width/2) {
+    fill(19,52,146); //azul
+}
+
+if(mouseY < height/2 && mouseX <= width/2) {
+    fill(221,56,56); //vermelho
+
+}
+    stroke(230);
+    strokeWeight(2);
+   
+    circle(width / 2, height / 2, height / 4); // circulo sempre ao centro do encrã
 
 
 }
